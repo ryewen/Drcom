c28a77795a431d9b04de4175c72672d80b0d1f45
ryewen <499647879@qq.com>
2017-09-01 16:03:56 +0800

    Update python_install.sh

--- python_install.sh
+++ python_install.sh
@@ -2,8 +2,8 @@
 
 opkg update
 
-opkg install python-openssl python-codecs
-
 opkg remove git
+
+opkg install python-openssl python-codecs
 
 rm -rf /opt/app/* /opt/etc/* /opt/lib/libcurl* /opt/sbin/* /opt/var/* /opt/bin/curl /opt/bin/find /opt/bin/opkg /opt/bin/xargs /opt/share/*
