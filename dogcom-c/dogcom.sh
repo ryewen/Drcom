fc9f8faa4092e063721228b7fc8e99aa1762c6ba
ryewen <499647879@qq.com>
2018-03-07 22:36:32 +0800

    c-version

---
+++ dogcom-c/dogcom.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+#!/bin/sh
+
+cmd='/tmp/dogcom_padavan'
+
+ps |while read str
+do
+        #echo $str
+        i=0
+        pid=0
+        equal=0
+        for word in $str
+        do
+                if [ $i -eq 0 ]
+                then
+                        pid=$word
+                fi
+                if [ "$word" = "$cmd" ]
+                then
+                        echo "kill $pid"
+                        kill -9 $pid
+                fi
+                i=$(($i + 1))
+        done
+done
+
+cmd='/tmp/dogcom_padavan -m dhcp -c /tmp/dogcom.conf'
+
+$cmd &
