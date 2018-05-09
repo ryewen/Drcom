0508d0237d1575c5fc6735286142fc2d19935644
499647879 <499647879@qq.com>
2018-05-09 18:31:25 +0800

    merlin

---
+++ dogcom-merlin/dogcom.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+dir='/etc/storage/'
+app='dogcom_padavan_once'
+cmd=$dir$app
+
+ps |while read str
+do
+        #echo $str
+        i=0
+        pid=0
+        equal=0
+        for word in $str
+        do
+                if [ $i -eq 0 ]
+                then
+                        pid=$word
+                fi
+                if [ "$word" = "$cmd" ]
+                then
+                        echo "kill $pid"
+                        kill -9 $pid
+                fi
+                i=$(($i + 1))
+        done
+done
+
+cmd=$cmd' -m dhcp -c '$dir'dogcom.conf'
+
+$cmd &
